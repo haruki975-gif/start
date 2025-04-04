0333bb1138b95a9390909b694ac1d601037f6a1b
haruki975-gif <haruki975@gmail.com>
2025-04-04 17:51:03 +0900

    boot

--- src/main/java/com/kh/start/comments/controller/CommentController.java
+++ src/main/java/com/kh/start/comments/controller/CommentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.start.comments.model.dto.CommentDTO;
@@ -37,9 +38,9 @@ public class CommentController {
 	
 	@GetMapping
 	public ResponseEntity<List<CommentDTO>> selectCommentList(
-											@RequestBody Map<String, String> request){
+											@RequestParam(name="boardNo") String boardNo){
 		return ResponseEntity.ok(
-				commentService.selectCommentList(Long.parseLong(request.get("boardNo"))));
+				commentService.selectCommentList(Long.parseLong(boardNo)));
 	}
 	
 	
